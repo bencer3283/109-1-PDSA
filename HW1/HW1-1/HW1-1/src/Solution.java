a8cc2169ff66ad726df83c670bc39775d8da068e
CHENG PO SHENG <bencer@outlook.com>
2020-09-21 23:43:04 +0800

    env done

---
+++ HW1/HW1-1/HW1-1/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[]{1,3};
+    }
+
+    public static void main(String []args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.twoSum(new int[]{2,7,11,15}, 26)));
+    }
+}
