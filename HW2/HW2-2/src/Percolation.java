6be5db155ee444eef3cddeb6d94e876072f41173
CHENG PO SHENG <bencer@outlook.com>
2020-10-12 02:04:12 +0800

    fatest achived w/ RE

--- HW2/HW2-2/src/Percolation.java
+++ HW2/HW2-2/src/Percolation.java
@@ -1,13 +1,11 @@
 
-import java.util.HashMap;
-
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     int n;
     WeightedQuickUnionUF gridConnect;
-    HashMap<Integer, Integer> openedSites = new HashMap<Integer, Integer>();
+    //HashMap<Integer, Integer> openedSites = new HashMap<Integer, Integer>();
+    boolean[] grid;
 
     public Percolation(int N) {
         // create N-by-N grid, with all sites blocked
@@ -15,68 +13,70 @@ public class Percolation {
         // for(int i = 0; i < grid.length; i++) grid[i]=false;
         gridConnect = new WeightedQuickUnionUF(N*N);
         n=N;
+        grid = new boolean[N*N];
+        for(int i = 0; i < grid.length; i++){
+            grid[i] = false;
+        }
     }
 
     public void open(int row, int column) {
         // open site (row , column) if it is not open already
         int pos = n*(row) + column;
-        if(!openedSites.containsKey(pos)){
-            //connect();
-            int left = n*row + column - 1;
-            int right = n*row + column + 1;
-            int top = n*(row-1) + column;
-            int bottom = n*(row+1) + column;
-            if(row == 0 && column != 0 && column != n-1){
-                if(openedSites.containsKey(left)) gridConnect.union(left, pos);
-                if(openedSites.containsKey(right)) gridConnect.union(right, pos);
-                if(openedSites.containsKey(bottom)) gridConnect.union(bottom, pos);
-            }
-            else if(row == n-1 && column != 0 && column != n-1){
-                if(openedSites.containsKey(left)) gridConnect.union(left, pos);
-                if(openedSites.containsKey(right)) gridConnect.union(right, pos);
-                if(openedSites.containsKey(top)) gridConnect.union(top, pos);
-            }
-            else if(column == 0 && row != 0 && row != n-1){
-                if(openedSites.containsKey(right)) gridConnect.union(right, pos);
-                if(openedSites.containsKey(top)) gridConnect.union(top, pos);
-                if(openedSites.containsKey(bottom)) gridConnect.union(bottom, pos);
-            }
-            else if(column == n-1 && row != 0 && row != n-1){
-                if(openedSites.containsKey(left)) gridConnect.union(left, pos);
-                if(openedSites.containsKey(top)) gridConnect.union(top, pos);
-                if(openedSites.containsKey(bottom)) gridConnect.union(bottom ,pos);
-            }
-            else if(pos == 0){
-                if(openedSites.containsKey(right)) gridConnect.union(right, pos);
-                if(openedSites.containsKey(bottom)) gridConnect.union(bottom, pos);
-            }
-            else if(pos == n-1){
-                if(openedSites.containsKey(left)) gridConnect.union(left, pos);
-                if(openedSites.containsKey(bottom)) gridConnect.union(bottom, pos);
-            }
-            else if(pos == (n-1)*n){
-                if(openedSites.containsKey(right)) gridConnect.union(right, pos);
-                if(openedSites.containsKey(top)) gridConnect.union(top, pos);
-            }
-            else if(pos == (n*n)-1){
-                if(openedSites.containsKey(left)) gridConnect.union(left, pos);
-                if(openedSites.containsKey(top)) gridConnect.union(top, pos); 
-            }
-            else{
-                if(openedSites.containsKey(left)) gridConnect.union(left, pos);
-                if(openedSites.containsKey(right)) gridConnect.union(right, pos);
-                if(openedSites.containsKey(top)) gridConnect.union(top, pos);
-                if(openedSites.containsKey(bottom)) gridConnect.union(bottom, pos);
-            }
-            openedSites.put(pos, gridConnect.find(pos));
-        }        
+        grid[pos] = true;
+        //connect();
+        int left = n*row + column - 1;
+        int right = n*row + column + 1;
+        int top = n*(row-1) + column;
+        int bottom = n*(row+1) + column;
+        if(row == 0 && column != 0 && column != n-1){
+            if(grid[left]) gridConnect.union(left, pos);
+            if(grid[right]) gridConnect.union(right, pos);
+            if(grid[bottom]) gridConnect.union(bottom, pos);
+        }
+        else if(row == n-1 && column != 0 && column != n-1){
+            if(grid[left]) gridConnect.union(left, pos);
+            if(grid[right]) gridConnect.union(right, pos);
+            if(grid[top]) gridConnect.union(top, pos);
+        }
+        else if(column == 0 && row != 0 && row != n-1){
+            if(grid[right]) gridConnect.union(right, pos);
+            if(grid[top]) gridConnect.union(top, pos);
+            if(grid[bottom]) gridConnect.union(bottom, pos);
+        }
+        else if(column == n-1 && row != 0 && row != n-1){
+            if(grid[left]) gridConnect.union(left, pos);
+            if(grid[top]) gridConnect.union(top, pos);
+            if(grid[bottom]) gridConnect.union(bottom ,pos);
+        }
+        else if(pos == 0){
+            if(grid[right]) gridConnect.union(right, pos);
+            if(grid[bottom]) gridConnect.union(bottom, pos);
+        }
+        else if(pos == n-1){
+            if(grid[left]) gridConnect.union(left, pos);
+            if(grid[bottom]) gridConnect.union(bottom, pos);
+        }
+        else if(pos == (n-1)*n){
+            if(grid[right]) gridConnect.union(right, pos);
+            if(grid[top]) gridConnect.union(top, pos);
+        }
+        else if(pos == (n*n)-1){
+            if(grid[left]) gridConnect.union(left, pos);
+            if(grid[top]) gridConnect.union(top, pos); 
+        }
+        else{
+            if(grid[left]) gridConnect.union(left, pos);
+            if(grid[right]) gridConnect.union(right, pos);
+            if(grid[top]) gridConnect.union(top, pos);
+            if(grid[bottom]) gridConnect.union(bottom, pos);
+        }
+            
     }
 
     public boolean isOpen(int i, int j) {
         // is site (row i, column j) open?
         int pos = n*i+j;
-        if(openedSites.containsKey(pos)) return true;
-        else return false;
+        return grid[pos];
     }
 
     public boolean isFull(int i, int j) {
