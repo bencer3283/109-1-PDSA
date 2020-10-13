650aea31a65b46dd50a093e922f0d69c0764ae5c
CHENG PO SHENG <bencer@outlook.com>
2020-10-14 00:48:24 +0800

    case5 full point

--- HW2/HW2-1/src/BoardGame.java
+++ HW2/HW2-1/src/BoardGame.java
@@ -5,93 +5,155 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class BoardGame {
-    HashMap<Integer, Integer> stoneOrder = new HashMap<Integer, Integer>();
+    //HashMap<Integer, Integer> stoneOrder = new HashMap<Integer, Integer>();
     HashMap<Integer, Boolean> stoneType = new HashMap<Integer, Boolean>();
-    HashMap<Integer, Integer> stonePos = new HashMap<Integer,Integer>();
-    HashMap<Integer, ArrayList<Integer>> stoneFamily = new HashMap<Integer, ArrayList<Integer>>();
+    //HashMap<Integer, Integer> stonePos = new HashMap<Integer,Integer>();
+    //HashMap<Integer, ArrayList<Integer>> stoneFamily = new HashMap<Integer, ArrayList<Integer>>();
+    HashMap<Integer, Integer> familySides = new HashMap<Integer, Integer>();
     WeightedQuickUnionUF stoneConnect;
     int size = 0;
     int board = 0;
+    int height = 0;
+    int width = 0;
         
     public BoardGame(int h, int w) {
         // create a board of size h*w
         //ArrayList<ArrayList<String>> board = new ArrayList<>();
-        stoneConnect = new WeightedQuickUnionUF(h*w);
         board = h*w;
+        stoneConnect = new WeightedQuickUnionUF(board);
+        height = h;
+        width = w;
         
     }
     
     public void putStone(int[] x, int[] y, char Type) {
         // put stones on the board according to the coordinate
         for(int i = 0; i < x.length; i++){
-            int pos = x[i]*100000+y[i];
+            int pos = y[i]+x[i]*100000;
+            int index = y[i]*width+x[i];
             boolean type = false;
-            int order = size + i;
-            if(Type == 'O'){
-                type = true;
-            }
+            //int order = size + i;
+            if(Type == 'O') type = true;
             stoneType.put(pos, type);
-            stoneOrder.put(pos, order);
-            stonePos.put(order, pos);
+            familySides.put(index, 4);
+            //stoneOrder.put(pos, order);
+            //stonePos.put(order, pos);
             int left = (x[i]-1)*100000+y[i];
+            int indexLeft = (left%100000)*width + (left/100000);
             int right = (x[i]+1)*100000+y[i];
+            int indexRight = (right%100000)*width + (right/100000);
             int top = x[i]*100000+y[i]+1;
+            int indexTop = (top%100000)*width + (top/100000);
             int bottom = x[i]*100000+y[i]-1;
+            int indexBottom = (bottom%100000)*width + (bottom/100000);
+            int thisfamily = stoneConnect.find(index);
+
             if(stoneType.containsKey(left)){
+                int compareToFamily = stoneConnect.find(indexLeft);
                 if(stoneType.get(left)==stoneType.get(pos)){
-                    stoneConnect.union(stoneOrder.get(pos), stoneOrder.get(left));
+                    if(thisfamily == compareToFamily){
+                        familySides.put(thisfamily, familySides.get(thisfamily)-2);
+                    }
+                    else{
+                        int sides = familySides.get(thisfamily) + familySides.get(compareToFamily) - 2;
+                        stoneConnect.union(index, indexLeft);
+                        familySides.put(thisfamily, sides);
+                    }
+                }
+                else{
+                    // sides --
+                    familySides.put(compareToFamily, familySides.get(compareToFamily)-1);
                 }
             }
             if(stoneType.containsKey(right)){
-                if(stoneType.get(right)==stoneType.get(pos)){
-                    stoneConnect.union(stoneOrder.get(pos), stoneOrder.get(right));
+                int compareToFamily = stoneConnect.find(indexRight);
+                if(stoneType.get(left)==stoneType.get(pos)){
+                    if(thisfamily == compareToFamily){
+                        familySides.put(thisfamily, familySides.get(thisfamily)-2);
+                    }
+                    else{
+                        int sides = familySides.get(thisfamily) + familySides.get(compareToFamily) - 2;
+                        stoneConnect.union(index, indexRight);
+                        familySides.put(thisfamily, sides);
+                    }
+                }
+                else{
+                    // sides --
+                    familySides.put(compareToFamily, familySides.get(compareToFamily)-1);
                 }
             }
             if(stoneType.containsKey(top)){
-                if(stoneType.get(top)==stoneType.get(pos)){
-                    stoneConnect.union(stoneOrder.get(pos), stoneOrder.get(top));
+                int compareToFamily = stoneConnect.find(indexTop);
+                if(stoneType.get(left)==stoneType.get(pos)){
+                    if(thisfamily == compareToFamily){
+                        familySides.put(thisfamily, familySides.get(thisfamily)-2);
+                    }
+                    else{
+                        int sides = familySides.get(thisfamily) + familySides.get(compareToFamily) - 2;
+                        stoneConnect.union(index, indexTop);
+                        familySides.put(thisfamily, sides);
+                    }
+                }
+                else{
+                    // sides --
+                    familySides.put(compareToFamily, familySides.get(compareToFamily)-1);
                 }
             }
             if(stoneType.containsKey(bottom)){
-                if(stoneType.get(bottom)==stoneType.get(pos)){
-                    stoneConnect.union(stoneOrder.get(pos), stoneOrder.get(bottom));
+                int compareToFamily = stoneConnect.find(indexBottom);
+                if(stoneType.get(left)==stoneType.get(pos)){
+                    if(thisfamily == compareToFamily){
+                        familySides.put(thisfamily, familySides.get(thisfamily)-2);
+                    }
+                    else{
+                        int sides = familySides.get(thisfamily) + familySides.get(compareToFamily) - 2;
+                        stoneConnect.union(index, indexBottom);
+                        familySides.put(thisfamily, sides);
+                    }
+                }
+                else{
+                    // sides --
+                    familySides.put(compareToFamily, familySides.get(compareToFamily)-1);
                 }
             }
-        
         }
 
-        size = size + x.length;
+        // size = size + x.length;
 
-        for(int i = 0; i < size; i++){
-            int canonical = stoneConnect.find(i);
-            if(stoneFamily.containsKey(canonical)){
-                stoneFamily.get(canonical).add(i);
-            }
-            else{
-                ArrayList<Integer> family = new ArrayList<Integer>();
-                stoneFamily.put(canonical, family);
-                stoneFamily.get(canonical).add(i);
-            }
-        }
+        // for(int i = 0; i < size; i++){
+        //     int canonical = stoneConnect.find(i);
+        //     if(stoneFamily.containsKey(canonical)){
+        //         stoneFamily.get(canonical).add(i);
+        //     }
+        //     else{
+        //         ArrayList<Integer> family = new ArrayList<Integer>();
+        //         stoneFamily.put(canonical, family);
+        //         stoneFamily.get(canonical).add(i);
+        //     }
+        // }
     }
 
     public boolean surrounded(int x, int y) {
         // Answer if the stone and its connected stones are surrounded by another type of stones
-        int checkPos = x*100000+y;
-        int checkOrder = stoneOrder.get(checkPos);
-        int checkCanonical = stoneConnect.find(checkOrder);
-        ArrayList<Integer> checkFamily = stoneFamily.get(checkCanonical);
-        for (int i = 0; i < checkFamily.size(); i++){
-            int current = stonePos.get(checkFamily.get(i));
-            int left =  current - 100000;
-            int right = current + 100000;
-            int top = current + 1;
-            int bottom = current - 1;
-            if(stoneType.containsKey(left) == false || stoneType.containsKey(right) == false || stoneType.containsKey(top) == false || stoneType.containsKey(bottom) == false){
-                return false;
-            }  
-        }
-        return true;
+        boolean sides = familySides.get(stoneConnect.find(y*width+x)) == 0;
+        if(sides) return true;
+        else return false;
+
+        // int checkPos = x*100000+y;
+        // int checkOrder = stoneOrder.get(checkPos);
+        // int checkCanonical = stoneConnect.find(checkOrder);
+        // ArrayList<Integer> checkFamily = stoneFamily.get(checkCanonical);
+        // for (int i = 0; i < checkFamily.size(); i++){
+        //     int current = stonePos.get(checkFamily.get(i));
+        //     int left =  current - 100000;
+        //     int right = current + 100000;
+        //     int top = current + 1;
+        //     int bottom = current - 1;
+        //     if(stoneType.containsKey(left) == false || stoneType.containsKey(right) == false || stoneType.containsKey(top) == false || stoneType.containsKey(bottom) == false){
+        //         return false;
+        //     }  
+        // }
+        // return true;
     }
 
     public char getStoneType(int x, int y) {
