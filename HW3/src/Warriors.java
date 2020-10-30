faee65f41ce7bba62b9cd660922b1eaab359d84a
Cheng Po Sheng <bencer@outlook.com>
2020-10-30 15:07:54 +0800

    10/30: HW3-1/2 both score 100

--- HW3/src/Warriors.java
+++ HW3/src/Warriors.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import edu.princeton.cs.algs4.Stack;
 
@@ -8,10 +7,10 @@ public class Warriors {
         //Given the attributes of each warriors and output the minimal and maximum 
         //index of warrior can be attacked by each warrior.
         int[] ans = new int[strength.length*2];
-        ans[0] = 0;
-        ans[strength.length*2 - 1] = strength.length - 1;
+        for(int i = 0; i < strength.length; i++) ans[i*2+1] = strength.length - 1;
         Stack<Integer> fromLeft = new Stack<Integer>();
         Stack<Integer> fromRight = new Stack<Integer>();
+
         for(int i = 0; i < strength.length; i++){
             while(fromLeft.size() != 0 && strength[i] >= strength[fromLeft.peek()]){
                 ans[fromLeft.pop()*2+1] = i - 1;
@@ -28,6 +27,8 @@ public class Warriors {
             if(ans[2*i] < i - range[i]) ans[2*i] = i - range[i];
             if(ans[2*i+1] > i + range[i]) ans[2*i+1] = i + range[i];
         }
+        ans[0] = 0;
+        ans[strength.length*2 - 1] = strength.length - 1;
         if(strength.length == 1) return new int[] {0, 0};
         else return ans;
     }
@@ -35,8 +36,8 @@ public class Warriors {
     public static void main(String[] args) {
         Warriors sol = new Warriors();
         System.out.println(Arrays.toString(
-            sol.warriors(new int[] {11, 13, 11, 11, 7, 15},
-                        new int[] { 1,  8,  3, 1, 7,  2})));
-        // Output: [0, 0, 0, 4, 2, 2, 3, 4, 4, 4, 3, 5]
+            sol.warriors(new int[] {11, 13, 11, 11, 7, 15, 8, 8, 8, 9, 9, 9, 17, 6, 8, 20, 21, 20, 18, 14, 6, 5, 1},
+                        new int[] {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100})));
+        // Output: [0, 0, 0, 4, 2, 2, 3, 4, 4, 4, 0, 11, 6, 6, 7, 7, 8, 8, 6, 9, 10, 10, 11, 11, 0, 14, 13, 13, 13, 14, 0, 15, 0, 22, 17, 22, 18, 22, 19, 22, 20, 22, 21, 22, 22, 22]
     }
 }
