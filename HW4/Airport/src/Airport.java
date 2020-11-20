6d2c542a89ba9ae2d7be3b5542b98e54dab86014
Cheng Po Sheng <bencer@outlook.com>
2020-11-20 23:14:34 +0800

    same polar angle order problem existed

--- HW4/Airport/src/Airport.java
+++ HW4/Airport/src/Airport.java
@@ -1,36 +1,14 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.lang.Math;
 import edu.princeton.cs.algs4.Point2D;
 
-class House implements Comparable<House>{
-    public int x = 0;
-    public int y = 0;
-    public double angle = 0;
-
-    @Override
-    public int compareTo(House that) {
-        if(this.y != that.y)return this.y-that.y;
-        else return this.x-that.x; 
-    }
-
-    public static class polarComparator implements Comparator<House>{
-        @Override
-        public int compare(House one, House two){
-            if(Double.compare(one.angle, two.angle) != 0) return Double.compare(one.angle, two.angle);
-            else return two.y - one.y;
-        }
-    }
-}
-
-
 class Airport {
-    private int check_CCW(Point2D house_i, List<House> hull, List<House> inner){
+    private int check_CCW(Point2D house_i, List<Point2D> hull, List<Point2D> inner){
         int last_hull_index = hull.size()-1;
-        Point2D last_hull = new Point2D(hull.get(last_hull_index).x, hull.get(last_hull_index).y);
-        Point2D last_last_hull = new Point2D(hull.get(last_hull_index-1).x, hull.get(last_hull_index-1).y);
+        Point2D last_hull = hull.get(last_hull_index);
+        Point2D last_last_hull = hull.get(last_hull_index-1);
         int ccw = Point2D.ccw(last_last_hull, last_hull, house_i);
         if(ccw == -1){
             inner.add(hull.get(last_hull_index));
@@ -42,55 +20,39 @@ class Airport {
     public double airport(List<int[]> houses) {
         // Output smallest average distance with optimal selection of airport location.
         double avg_dis = 0;
-        ArrayList<House> village = new ArrayList<House>();
-        ArrayList<House> hull = new ArrayList<House>();
-        ArrayList<House> inner = new ArrayList<House>();
-        ArrayList<House> between = new ArrayList<House>();
+        ArrayList<Point2D> village = new ArrayList<Point2D>();
+        ArrayList<Point2D> hull = new ArrayList<Point2D>();
+        ArrayList<Point2D> inner = new ArrayList<Point2D>();
+        ArrayList<Point2D> between = new ArrayList<Point2D>();
         for(int i = 0; i < houses.size(); i++){
-            House housetoadd = new House();
-            housetoadd.x = houses.get(i)[0];
-            housetoadd.y = houses.get(i)[1];
+            Point2D housetoadd = new Point2D(houses.get(i)[0], houses.get(i)[1]);
             village.add(housetoadd);
         }
         // find convex hull
-        Collections.sort(village); //sort by y
-        House origin = village.get(0);
-        village.remove(0);
+        Point2D origin = village.get(0);
         for(int i = 0; i < village.size(); i++){
-            double delX = village.get(i).x - origin.x;
-            double delY = village.get(i).y - origin.y;
-            if(delX == 0) village.get(i).angle = 90;
-            else if(delY == 0){
-                if(delX > 0) village.get(i).angle = 0;
-                if(delX < 0) village.get(i).angle = 180;
-            }
-            else{
-                double tan = delY/delX;
-                village.get(i).angle = Math.toDegrees(Math.atan(tan));
-                if(village.get(i).angle < 0) village.get(i).angle += 180;
-            }
+            if(village.get(i).y() < origin.y()) origin = village.get(i);
         }
-        Collections.sort(village, new House.polarComparator());
-        hull.add(origin);
+        Collections.sort(village, origin.polarOrder());
         hull.add(village.get(0));
-        for(int i = 1; i < village.size(); i++){
-            Point2D house_i = new Point2D(village.get(i).x, village.get(i).y);
-            check_CCW(house_i, hull, inner);
+        hull.add(village.get(1));
+        for(int i = 2; i < village.size(); i++){
+            check_CCW(village.get(i), hull, inner);
             hull.add(village.get(i));
         }
         for(int i = 1; i < hull.size()-1; i++){
-            Point2D me = new Point2D(hull.get(i).x, hull.get(i).y);
-            Point2D prior = new Point2D(hull.get(i-1).x, hull.get(i-1).y);
-            Point2D after= new Point2D(hull.get(i+1).x, hull.get(i+1).y);
+            Point2D me = hull.get(i);
+            Point2D prior = hull.get(i-1);
+            Point2D after= hull.get(i+1);
             if(Point2D.ccw(prior, me, after) == 0){
                 between.add(hull.get(i));
                 hull.remove(i);
                 i--;
             }
         }
-        Point2D me = new Point2D(hull.get(hull.size()-1).x, hull.get(hull.size()-1).y);
-        Point2D prior = new Point2D(hull.get(hull.size()-2).x, hull.get(hull.size()-2).y);
-        Point2D after= new Point2D(hull.get(0).x, hull.get(0).y);
+        Point2D me = hull.get(hull.size()-1);
+        Point2D prior = hull.get(hull.size()-2);
+        Point2D after= hull.get(0);
         if(Point2D.ccw(prior, me, after) == 0){
             between.add(hull.get(hull.size()-1));
             hull.remove(hull.size()-1);
@@ -99,40 +61,40 @@ class Airport {
         double equiX = 0;
         double equiY = 0;
         for (int i = 0; i < inner.size(); i++){
-            double x = inner.get(i).x;
-            double y = inner.get(i).y;
+            double x = inner.get(i).x();
+            double y = inner.get(i).y();
             equiX = equiX + (x / inner.size());
             equiY = equiY + (y / inner.size());
         }
         // for each possible runway calculate the average distance
-        double lx = hull.get(hull.size()-1).x - hull.get(0).x;
-        double ly = hull.get(hull.size()-1).y - hull.get(0).y;
-        double k = lx*hull.get(0).y - ly*hull.get(0).x;
+        double lx = hull.get(hull.size()-1).x() - hull.get(0).x();
+        double ly = hull.get(hull.size()-1).y() - hull.get(0).y();
+        double k = lx*hull.get(0).y() - ly*hull.get(0).x();
         for(int i = 0; i < hull.size(); i++){
-            double px = hull.get(i).x;
-            double py = hull.get(i).y;
+            double px = hull.get(i).x();
+            double py = hull.get(i).y();
             avg_dis += Math.abs((px*ly-py*lx+k)/Math.sqrt(lx*lx+ly*ly));
         }
         for(int i = 0; i < between.size(); i++){
-            double px = between.get(i).x;
-            double py = between.get(i).y;
+            double px = between.get(i).x();
+            double py = between.get(i).y();
             avg_dis += Math.abs((px*ly-py*lx+k)/Math.sqrt(lx*lx+ly*ly));
         }
         avg_dis += Math.abs((equiX*ly-equiY*lx+k)/Math.sqrt(lx*lx+ly*ly))*inner.size();
         avg_dis /= (village.size()+1);
         for(int i = 1; i < hull.size()-1; i++){
             double dis = 0;
-            lx = hull.get(i).x - hull.get(i+1).x;
-            ly = hull.get(i).y - hull.get(i+1).y;
-            k = lx*hull.get(i).y - ly*hull.get(i).x;
+            lx = hull.get(i).x() - hull.get(i+1).x();
+            ly = hull.get(i).y() - hull.get(i+1).y();
+            k = lx*hull.get(i).y() - ly*hull.get(i).x();
             for(int j = 0; j < hull.size(); j++){
-                double px = hull.get(j).x;
-                double py = hull.get(j).y;
+                double px = hull.get(j).x();
+                double py = hull.get(j).y();
                 dis += Math.abs((px*ly-py*lx+k)/Math.sqrt(lx*lx+ly*ly));
             }
             for(int j = 0; j < between.size(); j++){
-                double px = between.get(j).x;
-                double py = between.get(j).y;
+                double px = between.get(j).x();
+                double py = between.get(j).y();
                 dis += Math.abs((px*ly-py*lx+k)/Math.sqrt(lx*lx+ly*ly));
             }
             dis += Math.abs((equiX*ly-equiY*lx+k)/Math.sqrt(lx*lx+ly*ly))*inner.size();
