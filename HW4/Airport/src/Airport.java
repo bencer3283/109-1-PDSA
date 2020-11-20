56911d7fdea37722082c412f26c11cff87a2065e
Cheng Po Sheng <bencer@outlook.com>
2020-11-20 13:29:50 +0800

    finding convex hull succeeded(keep colinear element)

--- HW4/Airport/src/Airport.java
+++ HW4/Airport/src/Airport.java
@@ -26,34 +26,69 @@ class House implements Comparable<House>{
 
 
 class Airport {
+    private int check_CCW(Point2D house_i, List<House> hull){
+        int last_hull_index = hull.size()-1;
+        Point2D last_hull = new Point2D(hull.get(last_hull_index).x, hull.get(last_hull_index).y);
+        Point2D last_last_hull = new Point2D(hull.get(last_hull_index-1).x, hull.get(last_hull_index-1).y);
+        if(Point2D.ccw(last_last_hull, last_hull, house_i) == -1){
+            hull.remove(last_hull_index);
+            return check_CCW(house_i, hull);
+        }
+        else{
+            return 0;
+        }
+    }
     public double airport(List<int[]> houses) {
         // Output smallest average distance with optimal selection of airport location.
         double avg_dis = 0;
         ArrayList<House> village = new ArrayList<House>();
+        ArrayList<House> hull = new ArrayList<House>();
         for(int i = 0; i < houses.size(); i++){
-            village.get(i).x = houses.get(i)[0];
-            village.get(i).y = houses.get(i)[1];
+            House housetoadd = new House();
+            housetoadd.x = houses.get(i)[0];
+            housetoadd.y = houses.get(i)[1];
+            village.add(housetoadd);
         }
         Collections.sort(village); //sort by y
-        House origin = village.get(village.size()-1);
+        House origin = village.get(0);
+        village.remove(0);
         for(int i = 0; i < village.size(); i++){
             double delX = village.get(i).x - origin.x;
             double delY = village.get(i).y - origin.y;
-            double tan = Math.toRadians(delX/delY);
-            village.get(i).angle = Math.toDegrees(Math.atan(tan));
+            if(delX == 0) village.get(i).angle = 90;
+            else if(delY == 0){
+                if(delX > 0) village.get(i).angle = 0;
+                if(delX < 0) village.get(i).angle = 180;
+            }
+            else{
+                double tan = delY/delX;
+                village.get(i).angle = Math.toDegrees(Math.atan(tan));
+                if(village.get(i).angle < 0) village.get(i).angle += 180;
+            }
         }
         Collections.sort(village, new House.polarComparator());
-
-        
+        hull.add(origin);
+        hull.add(village.get(0));
+        for(int i = 1; i < village.size(); i++){
+            Point2D house_i = new Point2D(village.get(i).x, village.get(i).y);
+            check_CCW(house_i, hull);
+            hull.add(village.get(i));
+        }
         return avg_dis; 
     }   
 
     public static void main(String[] args) {
         // More example are in Python template
         System.out.println(new Airport().airport(new ArrayList<int[]>(){{
-            add(new int[]{0,0});
-            add(new int[]{1,0});
-            add(new int[]{0,1});
+            add(new int[]{1,1});
+            add(new int[]{2,2});
+            add(new int[]{0,2});
+            add(new int[]{2,0});
+            add(new int[]{2,4});
+            add(new int[]{3,3});
+            add(new int[]{4,2});
+            add(new int[]{4,1});
+            add(new int[]{4,0});
         }}));
     }   
 }
